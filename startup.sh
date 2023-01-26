4982cb8cb856bbbf2c46eb05eddd9197da75876e
AndromedaX7 <38732113+AndromedaX7@users.noreply.github.com>
2023-01-26 20:59:51 +0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -2,6 +2,6 @@ cd $HOME
 curl -o ubuntu-init.zip https://raw.githubusercontent.com/AndromedaX7/ubuntu-init/main/ubuntu-init.zip
 unzip ubuntu-init.zip 
 chmod a+x ubuntu-init-software-sc.sh
-rm -rf ubuntu-init.zip
+# rm -rf ubuntu-init.zip
 rm -rf startup.sh
 ./ubuntu-init-software-sc.sh
